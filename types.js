ddb93bbb49241e68068b8e3170fb25c9cfaac030
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-09-08 20:19:35 +0600

    premitive non premitive

--- types.js
+++ types.js
@@ -1,8 +1,45 @@
 // console.log('okkk');
 // console.log(15);
+
+
+
+// premitive type value ,,like simple value 
+// premitive reference dhore rakhe 
 const a = 5;
 const b = true;
 const c = 12;
 const d = 'sakib'
+
+
+
+
+
+
+//  non premitive value 
+// non premitive reference dhore rakhe na
+const array = [778, 58, 74, 8]
 const object = { name: 'najmus sakib', id: 173002057 };
-console.log(typeof a, typeof b, typeof c, typeof d, typeof object)
+// console.log(typeof a, typeof b, typeof c, typeof d, typeof object)
+
+
+
+
+
+
+
+let x = 5;
+let y = x;
+console.log(x, y)
+y = 7;
+console.log(x, y)
+
+
+
+
+let p = { job: 'webDeveloper' };
+let q = p;
+q = { job: 'backend' }
+console.log(q);
+
+
+
