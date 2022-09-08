1d90aa080212c1453eae23a8af679b2217f70026
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-09-08 21:22:52 +0600

    truthy and folsy value

---
+++ truety-and-folsey.js
@@ -0,0 +1,18 @@
+// truthy
+
+
+
+const z = { class: 9 };
+if (!!z) {
+    console.log('yes this is truthy value')
+}
+
+
+
+
+
+// falsy
+// const x = null;
+// if (!x) {
+//     console.log('yes this is falsy')
+// }
---
+++ undefined.js
@@ -0,0 +1,12 @@
+/* 
+8 ways to get undefined 
+1. variable that is not initialized  will give undefined
+2.function with no return will give undefined
+3.paremeter thatb is not pass will be undefined
+4.if return has nothing on the right side  will return undefined
+5.property that doesnt exists on an object  willl give you undefined
+6.Array elements outside  of the index range will give you undefined 
+7.deleting an element inside in array 
+8.set a value directly undefined
+
+*/
