3909c84e709bd21e6117ae196bbb6435ecd25b0d
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-09-08 20:07:22 +0600

    type

---
+++ types.js
@@ -0,0 +1,8 @@
+// console.log('okkk');
+// console.log(15);
+const a = 5;
+const b = true;
+const c = 12;
+const d = 'sakib'
+const object = { name: 'najmus sakib', id: 173002057 };
+console.log(typeof a, typeof b, typeof c, typeof d, typeof object)
